0fca59c46a1828b0e3fb3bbe8c52af9ed5286056
Batoy_Nicholo <nicholox08@gmail.com>
2026-07-11 09:19:31 +0800

    Update DeserializeMeals.cs

--- IT_ELECTIVE_2_PRELIM_EXAM_HttpClient/Exercises/DeserializeMeals.cs
+++ IT_ELECTIVE_2_PRELIM_EXAM_HttpClient/Exercises/DeserializeMeals.cs
@@ -1,37 +1,38 @@
-namespace IT_ELECTIVE_2_PRELIM_EXAM_HttpClient.Exercises;
+using System;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
 
-// EXERCISE 10: GET Deserialize Multiple Meals
-// TheMealDB API: https://themealdb.com/api/json/v1/1/search.php?f=a
-//
-// This endpoint returns ALL meals starting with the letter "a".
-//
-// Your task:
-// 1. Use the HttpClient to fetch meals starting with letter "a"
-// 2. Assert status code is 200 OK
-// 3. Parse the JSON and get the "meals" array
-// 4. Assert the array has more than 0 items
-// 5. Loop through each meal and print its name (strMeal)
-//
-// Response format:
-// {
-//   "meals": [
-//     { "idMeal": "52772", "strMeal": "Arrabiata", ... },
-//     { "idMeal": "52781", "strMeal": "Ayam Percik", ... },
-//     ...
-//   ]
-// }
-
-public class DeserializeMeals
+namespace IT_ELECTIVE_2_PRELIM_EXAM_HttpClient.Exercises
 {
-    public static async Task Run(HttpClient client)
+    public class DeserializeMeals
     {
-        // TODO: Send GET request to https://themealdb.com/api/json/v1/1/search.php?f=a
-        // TODO: Assert status code is 200 OK
-        // TODO: Parse the response JSON
-        // TODO: Get the "meals" array
-        // TODO: Assert the array has more than 0 items
-        // TODO: Loop through and print each meal's strMeal
+        public static async Task Run(HttpClient client)
+        {
+            string url = "https://www.themealdb.com/api/json/v1/1/search.php?f=a";
 
-        throw new NotImplementedException();
+            var response = await client.GetAsync(url);
+
+            response.EnsureSuccessStatusCode();
+
+            string body = await response.Content.ReadAsStringAsync();
+
+            using (JsonDocument doc = JsonDocument.Parse(body))
+            {
+                var mealsArray = doc.RootElement.GetProperty("meals");
+
+                if (mealsArray.GetArrayLength() > 0)
+                {
+                    foreach (JsonElement meal in mealsArray.EnumerateArray())
+                    {
+                        Console.WriteLine(meal.GetProperty("strMeal").GetString());
+                    }
+                }
+                else
+                {
+                    throw new Exception("The meals array is empty.");
+                }
+            }
+        }
     }
 }
