1b6b665207f4c9b7a174ba0919be86dad47679c9
Batoy_Nicholo <nicholox08@gmail.com>
2026-07-11 09:07:57 +0800

    Update GetCategories.cs

--- IT_ELECTIVE_2_PRELIM_EXAM_HttpClient/Exercises/GetCategories.cs
+++ IT_ELECTIVE_2_PRELIM_EXAM_HttpClient/Exercises/GetCategories.cs
@@ -1,24 +1,18 @@
-namespace IT_ELECTIVE_2_PRELIM_EXAM_HttpClient.Exercises;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
 
-// EXERCISE 4: GET List Categories
-// TheMealDB API: https://themealdb.com/api/json/v1/1/categories.php
-//
-// Your task:
-// 1. Use the HttpClient to fetch all meal categories
-// 2. Assert status code is 200 OK
-// 3. Parse the JSON and assert the "categories" array has more than 0 items
-//
-// Response format: { "categories": [{ "strCategory": "Beef", ... }, ...] }
-
-public static class GetCategories
+public class GetCategories
 {
     public static async Task Run(System.Net.Http.HttpClient client)
     {
-        // TODO: Send GET request to https://themealdb.com/api/json/v1/1/categories.php
-        // TODO: Assert status code is 200 OK
-        // TODO: Parse the response JSON
-        // TODO: Assert the "categories" array has more than 0 items
-
-        throw new NotImplementedException();
+        string url = "https://www.themealdb.com/api/json/v1/1/lookup.php?i=52771";
+        var response = await client.GetAsync(url);
+        string body = await response.Content.ReadAsStringAsync();
+        using (JsonDocument doc = JsonDocument.Parse(body))
+        {
+            var categoriesArray = doc.RootElement.GetProperty("meals");
+            int count = categoriesArray.GetArrayLength();
+        }
     }
 }
