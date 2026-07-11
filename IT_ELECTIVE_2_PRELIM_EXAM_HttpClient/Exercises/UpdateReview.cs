0cc278b82b296e4f86da4c1d5f0667f42ed7368c
Batoy_Nicholo <nicholox08@gmail.com>
2026-07-11 09:12:47 +0800

    Update UpdateReview.cs

--- IT_ELECTIVE_2_PRELIM_EXAM_HttpClient/Exercises/UpdateReview.cs
+++ IT_ELECTIVE_2_PRELIM_EXAM_HttpClient/Exercises/UpdateReview.cs
@@ -1,28 +1,21 @@
-namespace IT_ELECTIVE_2_PRELIM_EXAM_HttpClient.Exercises;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
 
-// EXERCISE 7: PUT Update Review
-// JSONPlaceholder API: https://jsonplaceholder.typicode.com/posts/{id}
-//
-// Your task:
-// 1. Create a JSON body: { "id": 1, "title": "Updated Review", "body": "Even better than before!", "userId": 1 }
-// 2. Wrap it in StringContent with media type "application/json"
-// 3. Send a PUT request to update post with ID 1
-// 4. Assert status code is 200 OK
-// 5. Parse the response JSON and assert the title is "Updated Review"
-//
-// Hint: Use await client.PutAsync(url, content)
-
-public static class UpdateReview
+public class UpdateReview
 {
-    public static async Task Run(System.Net.Http.HttpClient client)
+    public static async Task Run(HttpClient client)
     {
-        // TODO: Create JSON string with id, title, body, and userId
-        // TODO: Create StringContent with the JSON and Content-Type "application/json"
-        // TODO: Send PUT request to https://jsonplaceholder.typicode.com/posts/1
-        // TODO: Assert status code is 200 OK
-        // TODO: Parse the response JSON
-        // TODO: Assert the title is "Updated Review"
-
-        throw new NotImplementedException();
+        string url = "https://jsonplaceholder.typicode.com/posts/1";
+        string jsonBody = "{\"id\":1,\"title\":\"Updated Meal Review\",\"body\":\"Updated body text\",\"userId\":1}";
+        var content = new StringContent(jsonBody, Encoding.UTF8, "application/json");
+        var response = await client.PutAsync(url, content);
+        response.EnsureSuccessStatusCode();
+        string responseBody = await response.Content.ReadAsStringAsync();
+        using (JsonDocument doc = JsonDocument.Parse(responseBody))
+        {
+            var returnedTitle = doc.RootElement.GetProperty("title").GetString();
+        }
     }
 }
