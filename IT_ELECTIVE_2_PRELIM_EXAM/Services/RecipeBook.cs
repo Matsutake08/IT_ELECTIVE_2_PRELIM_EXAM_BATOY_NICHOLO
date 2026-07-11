f4a0e38066852bd7301c1009fd56b73697700b62
Batoy_Nicholo <nicholox08@gmail.com>
2026-07-11 08:32:55 +0800

    Update RecipeBook.cs

--- IT_ELECTIVE_2_PRELIM_EXAM/Services/RecipeBook.cs
+++ IT_ELECTIVE_2_PRELIM_EXAM/Services/RecipeBook.cs
@@ -7,12 +7,10 @@ public class RecipeBook
 {
     private List<Meal> _meals = new List<Meal>();
 
-    public string Name { get; set; }
+    public string Name { get; set; } = "";
     public int Capacity { get; set; }
 
-    public RecipeBook(string name) : this(name, 10)
-    {
-    }
+    public RecipeBook(string name) : this(name, 10) { }
 
     public RecipeBook(string name, int capacity)
     {
@@ -20,19 +18,9 @@ public class RecipeBook
         Capacity = capacity;
     }
 
-    public void AddMeal(Meal meal)
-    {
-        _meals.Add(meal);
-    }
+    public void AddMeal(Meal meal) => _meals.Add(meal);
 
-    public List<Meal> Search(string name, string category)
-    {
-        return _meals;
-    }
+    public List<Meal> Search(string name, string category) => _meals;
 
-    // Fixed: Changed parameter from 'int recipeId' to 'string text' to fix CS1503
-    public List<Meal> Search(string text)
-    {
-        return _meals;
-    }
+    public List<Meal> Search(string text) => _meals;
 }
