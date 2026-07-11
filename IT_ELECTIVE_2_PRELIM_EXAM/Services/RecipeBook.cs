1aa6200079f184f5fbd19ed6d986df63d7d5042c
Batoy_Nicholo <nicholox08@gmail.com>
2026-07-11 08:46:43 +0800

    Update RecipeBook.cs

--- IT_ELECTIVE_2_PRELIM_EXAM/Services/RecipeBook.cs
+++ IT_ELECTIVE_2_PRELIM_EXAM/Services/RecipeBook.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using IT_ELECTIVE_2_PRELIM_EXAM.Models;
 
@@ -20,7 +21,18 @@ public class RecipeBook
 
     public void AddMeal(Meal meal) => _meals.Add(meal);
 
-    public List<Meal> Search(string name, string category) => _meals;
+    public List<Meal> Search(string name, string category)
+    {
+        return _meals.FindAll(m =>
+            m.Name.Contains(name, StringComparison.OrdinalIgnoreCase) &&
+            m.Category.Contains(category, StringComparison.OrdinalIgnoreCase)
+        );
+    }
 
-    public List<Meal> Search(string text) => _meals;
+    public List<Meal> Search(string text)
+    {
+        return _meals.FindAll(m =>
+            m.Name.Contains(text, StringComparison.OrdinalIgnoreCase)
+        );
+    }
 }
