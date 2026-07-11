0ac3c2601ebe5e4e02117c2694320f553e26e240
Batoy_Nicholo <nicholox08@gmail.com>
2026-07-11 09:09:26 +0800

    Update FilterByIngredient.cs

--- IT_ELECTIVE_2_PRELIM_EXAM_HttpClient/Exercises/FilterByIngredient.cs
+++ IT_ELECTIVE_2_PRELIM_EXAM_HttpClient/Exercises/FilterByIngredient.cs
@@ -1,24 +1,18 @@
-namespace IT_ELECTIVE_2_PRELIM_EXAM_HttpClient.Exercises;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
 
-// EXERCISE 5: GET Filter by Ingredient
-// TheMealDB API: https://themealdb.com/api/json/v1/1/filter.php?i={ingredient}
-//
-// Your task:
-// 1. Use the HttpClient to filter meals by ingredient "chicken_breast"
-// 2. Assert status code is 200 OK
-// 3. Parse the JSON and assert the "meals" array has at least 1 item
-//
-// Response format: { "meals": [{ "strMeal": "...", "strMealThumb": "...", "idMeal": "..." }, ...] }
-
-public static class FilterByIngredient
+public class FilterByIngredient
 {
-    public static async Task Run(System.Net.Http.HttpClient client)
+    public static async Task Run(HttpClient client)
     {
-        // TODO: Send GET request to https://themealdb.com/api/json/v1/1/filter.php?i=chicken_breast
-        // TODO: Assert status code is 200 OK
-        // TODO: Parse the response JSON
-        // TODO: Assert the "meals" array has at least 1 item
-
-        throw new NotImplementedException();
+        string url = "https://www.themealdb.com/api/json/v1/1/lookup.php?i=52771";
+        var response = await client.GetAsync(url);
+        string body = await response.Content.ReadAsStringAsync();
+        using (JsonDocument doc = JsonDocument.Parse(body))
+        {
+            var mealsArray = doc.RootElement.GetProperty("meals");
+            int count = mealsArray.GetArrayLength();
+        }
     }
 }
