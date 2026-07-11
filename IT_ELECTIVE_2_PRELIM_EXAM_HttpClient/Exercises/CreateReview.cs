1afb09cded21613e9cac55f8f9e779cdce119a01
Batoy_Nicholo <nicholox08@gmail.com>
2026-07-11 09:11:35 +0800

    Update CreateReview.cs

--- IT_ELECTIVE_2_PRELIM_EXAM_HttpClient/Exercises/CreateReview.cs
+++ IT_ELECTIVE_2_PRELIM_EXAM_HttpClient/Exercises/CreateReview.cs
@@ -1,29 +1,20 @@
-namespace IT_ELECTIVE_2_PRELIM_EXAM_HttpClient.Exercises;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
 
-// EXERCISE 6: POST Create Review
-// JSONPlaceholder API: https://jsonplaceholder.typicode.com/posts
-//
-// Your task:
-// 1. Create a JSON body string: { "title": "Great Pasta!", "body": "Loved this recipe", "userId": 1 }
-// 2. Wrap it in StringContent with media type "application/json"
-// 3. Send a POST request to the URL
-// 4. Assert status code is 201 Created
-// 5. Parse the response JSON and assert it contains an "id" field
-//
-// Hint: Use await client.PostAsync(url, content)
-// Hint: Use new StringContent(json, Encoding.UTF8, "application/json")
-
-public static class CreateReview
+public class CreateReview
 {
-    public static async Task Run(System.Net.Http.HttpClient client)
+    public static async Task Run(HttpClient client)
     {
-        // TODO: Create JSON string with title, body, and userId
-        // TODO: Create StringContent with the JSON and Content-Type "application/json"
-        // TODO: Send POST request to https://jsonplaceholder.typicode.com/posts
-        // TODO: Assert status code is 201 Created
-        // TODO: Parse the response JSON
-        // TODO: Assert the response has an "id" field with a value
-
-        throw new NotImplementedException();
+        string url = "https://jsonplaceholder.typicode.com/posts";
+        string jsonBody = "{\"title\":\"Great Meal\",\"body\":\"It was delicious!\",\"userId\":1}";
+        var content = new StringContent(jsonBody, Encoding.UTF8, "application/json");
+        var response = await client.PostAsync(url, content);
+        string responseBody = await response.Content.ReadAsStringAsync();
+        using (JsonDocument doc = JsonDocument.Parse(responseBody))
+        {
+            var id = doc.RootElement.GetProperty("id").GetInt32();
+        }
     }
 }
